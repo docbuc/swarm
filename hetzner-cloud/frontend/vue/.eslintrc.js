98ece33d6415888b0d20b8ee11835946f55c54e6
Bernd Oeggl <bernd@komplett.cc>
2020-01-28 11:25:06 +0100

    final updates for v2

---
+++ hetzner-cloud/frontend/vue/.eslintrc.js
@@ -0,0 +1,17 @@
+// Datei: mwa/frontend/vue/.eslintrc.js
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+  rules: {
+    'no-console': 'off'
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
